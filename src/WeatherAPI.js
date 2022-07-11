db6e47190883d4c7706839245e860afe22c76bb8
MikkoK189 <mikkok11@gmail.com>
2022-07-11 19:02:54 +0300

    Refactoring to use onecall API.

--- src/WeatherAPI.js
+++ src/WeatherAPI.js
@@ -29,15 +29,24 @@ function DayWeather(day, mintemp, maxtemp) {
   this.maxtemp = "30";
 }
 
+async function getCoordinates(location) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`
+  );
+  const coordInfo = await response.json();
+
+  return coordInfo;
+}
+
 // Tries to call the weather from openweathermap API
 async function GetWeather(location) {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`
     );
-
     const weatherInfo = await response.json();
-    getForecast(location);
+    // getForecast(location);
+    getForecast();
     return parseWeather(weatherInfo);
   } catch (err) {
     console.error(err);
@@ -45,44 +54,53 @@ async function GetWeather(location) {
   }
 }
 
-async function getForecast(location) {
+async function getForecast() {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${key}`
+    `https://api.openweathermap.org/data/2.5/onecall?lon=23.0167&lat=62.0167&appid=2064d0a9d778095e3bf01bf1214f9eb3&exclude=minutely,alerts,hourly`
   );
-
   const weatherInfo = await response.json();
 
-  const weatherExceptToday = weatherInfo.list.filter(
-    (item) => !isToday(parseISO(item.dt_txt))
-  );
-
-  // Want to go through the array and separate weathers for each day into their own arrays from which to find min and max temp for each day.
-  for (let i = 0; i < 5; i++) {
-    // console.log(addDays(new Date(), i + 1));
-    let weatherDay = weatherExceptToday.filter(function (item) {
-      // console.log(parseISO(item.dt_txt));
-      let dayToCompare = addDays(new Date(), i + 1);
-      dayToCompare.setHours(0, 0, 0, 0);
-
-      let testDay = parseISO(item.dt_txt);
-      testDay.setHours(0, 0, 0, 0);
-
-      if (compareAsc(dayToCompare, testDay) == 0) {
-        return true;
-      }
-      return false;
-    });
-    weathersByDays.push(weatherDay);
-    console.log(weathersByDays);
-  }
-  // Gets max temp from the day's temperatures!!
-  const maxTemp = weathersByDays[0].reduce((previous, current) => {
-    if (current.main.temp > previous) return current.main.temp;
-    return previous;
-  }, 0);
-  console.log(maxTemp);
-  console.log(Math.max(weathersByDays[0][0].main.temp));
+  console.log(weatherInfo);
 }
+
+// async function getForecast(location) {
+//   const response = await fetch(
+//     `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${key}`
+//   );
+
+//   const weatherInfo = await response.json();
+
+//   const weatherExceptToday = weatherInfo.list.filter(
+//     (item) => !isToday(parseISO(item.dt_txt))
+//   );
+
+//   // Want to go through the array and separate weathers for each day into their own arrays from which to find min and max temp for each day.
+//   for (let i = 0; i < 5; i++) {
+//     // console.log(addDays(new Date(), i + 1));
+//     let weatherDay = weatherExceptToday.filter(function (item) {
+//       // console.log(parseISO(item.dt_txt));
+//       let dayToCompare = addDays(new Date(), i + 1);
+//       dayToCompare.setHours(0, 0, 0, 0);
+
+//       let testDay = parseISO(item.dt_txt);
+//       testDay.setHours(0, 0, 0, 0);
+
+//       if (compareAsc(dayToCompare, testDay) == 0) {
+//         return true;
+//       }
+//       return false;
+//     });
+//     weathersByDays.push(weatherDay);
+//     console.log(weathersByDays);
+//   }
+//   // Gets max temp from the day's temperatures!!
+//   const maxTemp = weathersByDays[0].reduce((previous, current) => {
+//     if (current.main.temp > previous) return current.main.temp;
+//     return previous;
+//   }, 0);
+//   console.log(maxTemp);
+//   console.log(Math.max(weathersByDays[0][0].main.temp));
+// }
 
 function compareDates(dateToCompare, dateComparedTo) {
   return isEqual(dateToCompare, dateComparedTo);
